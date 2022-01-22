3cd3aa012ad4413aa02a844bda67a84b93420eb5
Alia Zeenat <bitf18a522@pucit.edu.pk>
2022-01-22 22:29:17 +0500

    adding functions in showallfiles

--- TimeSaver/app/src/main/java/com/example/timesaver/ShowAllApps.java
+++ TimeSaver/app/src/main/java/com/example/timesaver/ShowAllApps.java
@@ -76,5 +76,16 @@ public class ShowAllApps extends AppCompatActivity {
         listView.setAdapter(showAllAppsAdapter);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+  
 
 }
