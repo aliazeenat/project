48c0002ff37c42001c307da87f79ab0481dd434b
Alia Zeenat <bitf18a522@pucit.edu.pk>
2022-01-23 01:06:31 +0500

    working on pageadapter

--- TimeSaver/app/src/main/java/com/example/timesaver/PagerAdapter.java
+++ TimeSaver/app/src/main/java/com/example/timesaver/PagerAdapter.java
@@ -22,5 +22,32 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
         this.NoOfTabs = noOfTabs;
    }
 
-  
+    @Override
+    public Fragment getItem(int position) {
+
+        if (fragmentHashMap.get(position) != null) {
+            return fragmentHashMap.get(position);
+        }
+        else {
+
+            switch (position) {
+                case 0:
+                    Today today = new Today();
+                    fragmentHashMap.put(position, today);
+                    return today;
+                case 1:
+                    Details details = new Details();
+                    fragmentHashMap.put(position, details);
+                    return details;
+                case 2:
+                    History history = new History();
+                    fragmentHashMap.put(position, history);
+                    return history;
+                default:
+                    return null;
+            }
+        }
+    }
+
+   
 }
