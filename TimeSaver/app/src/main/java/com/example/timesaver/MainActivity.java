587326efecdffd4e2764b1306f36c2e41cb074a6
Alia Zeenat <bitf18a522@pucit.edu.pk>
2022-01-22 18:51:51 +0500

    adding settheme function

--- TimeSaver/app/src/main/java/com/example/timesaver/MainActivity.java
+++ TimeSaver/app/src/main/java/com/example/timesaver/MainActivity.java
@@ -224,7 +224,18 @@ public class MainActivity extends AppCompatActivity implements Today.OnFragmentI
 
     }
 
-   
+    public void setTheme()
+    {
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("Mypref",0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        int theme = sharedPreferences.getInt("Theme",0);
+        if (theme == 0)
+            lightTheme();
+        else
+            darkTheme();
+
+    }
 
     public void SetAllAppsNamesAndIcons()
     {
