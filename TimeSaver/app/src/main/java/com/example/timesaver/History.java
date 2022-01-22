ac43b439d2ca5589bdf44af82d1581ab9aa2e32a
Alia Zeenat <bitf18a522@pucit.edu.pk>
2022-01-23 00:58:42 +0500

    adding history

---
+++ TimeSaver/app/src/main/java/com/example/timesaver/History.java
@@ -0,0 +1,47 @@
+package com.example.timesaver;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.PaintDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link History.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link History#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class History extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+ 
+
+
+}
