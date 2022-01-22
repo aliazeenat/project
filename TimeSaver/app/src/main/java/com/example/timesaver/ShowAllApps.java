662a40906033e9ad017dba4cd304d7bf48cda8fc
Alia Zeenat <bitf18a522@pucit.edu.pk>
2022-01-22 22:19:22 +0500

    adding show all files

--- TimeSaver/app/src/main/java/com/example/timesaver/ShowAllApps.java
+++ TimeSaver/app/src/main/java/com/example/timesaver/ShowAllApps.java
@@ -35,6 +35,46 @@ public class ShowAllApps extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_all_apps);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        setTheme();
+        //appInfoList = new ArrayList<>();
+        //Intent intent = getIntent();
+        //ArrayList<AppInfoList> appInfoList = (ArrayList<AppInfoList>) getIntent().getSerializableExtra("list");
+
+        final Calendar cal1 = Calendar.getInstance();
+        //cal1.add(Calendar.DAY_OF_MONTH, -1);
+        cal1.set(Calendar.HOUR_OF_DAY,00);
+        cal1.set(Calendar.MINUTE,00);
+        cal1.set(Calendar.SECOND,00);
+
+        final Calendar cal2 = Calendar.getInstance();
+        //cal2.add(Calendar.DAY_OF_MONTH, 1);
+        cal2.set(Calendar.HOUR_OF_DAY,23);
+        cal2.set(Calendar.MINUTE,59);
+        cal2.set(Calendar.SECOND,59);
+
+        UsageTime usageTime = new UsageTime(getApplicationContext(),cal1,cal2);
+        appInfoList = usageTime.getAppInfoList();
+
+        //Bundle extra = getIntent().getBundleExtra("extra");
+        //appInfoList = (ArrayList<AppInfoList>) extra.getSerializable("objects");
+
+        ShowAllAppsAdapter showAllAppsAdapter = new ShowAllAppsAdapter(this,R.layout.customlvallappstimes);
+        ListView listView = (ListView) findViewById(R.id.lvforapps);
+
+        for(int i = appInfoList.size()-1; i>=0 ; i--)
+        {
+            showAllAppsAdapter.add(appInfoList.get(i));
+
+        }
+
+        listView.setAdapter(showAllAppsAdapter);
+    }
 
 
 }
