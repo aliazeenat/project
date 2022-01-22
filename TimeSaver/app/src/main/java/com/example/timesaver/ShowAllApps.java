e0b0109f22dc9b7af42163b637eacfa2a99bb47b
Alia Zeenat <bitf18a522@pucit.edu.pk>
2022-01-22 22:18:09 +0500

    adding show all files

---
+++ TimeSaver/app/src/main/java/com/example/timesaver/ShowAllApps.java
@@ -0,0 +1,40 @@
+package com.example.timesaver;
+
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class ShowAllApps extends AppCompatActivity {
+
+    ArrayList<AppInfoList> appInfoList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_all_apps);
+
+
+
+}
